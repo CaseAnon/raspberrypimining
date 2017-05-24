cc538d98268dcfeb3d7d53fdf98309f0adadae87
Case <c4645843@trbvn.com>
2017-05-24 11:29:25 +0000

    Update minerSetup.sh

--- minerSetup.sh
+++ minerSetup.sh
@@ -3,12 +3,15 @@ if [ "$EUID" -ne 0 ]
 	then echo "Please run as root"
 else
 apt update -y
-apt install autoconf libcurl4-openssl-dev ibjansson-dev openssl libssl-dev gcc gawk automake git -y
+apt install autoconf libcurl4-openssl-dev ibjansson-dev openssl libssl-dev gcc gawk automake git screen -y
 git clone https://github.com/lucasjones/cpuminer-multi.git
 cd cpuminer-multi
-./autogen.sh
+./autogen.sh 
 ./configure
+
 make
+
+./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u anoncase@protonmail.com -p x -t 4
 printf "To use type ./minerd with any options you want while in the cpuminer-multi folder\n"
 printf "For example ./minerd -a cryptonight -o stratum+tcp://xmr-usa.dwarfpool.com:8050 -u 45MfScNnyb3KwanUH4feiDExdydQ3cACKPpJuxmtoVnY25Khw7nBeZmXhtoWj4Pfi4PjdxkFWpaW72PR3Jy1Xr8R6HFyk9M -p x -t 4"
 
